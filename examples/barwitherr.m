da6da2507c0e7aad08ee34c9fab8f9f4a76cd51f
María Pérez Ortiz <i82perom@uco.es>
2017-10-12 15:19:42 +0100

    New function to compare outliers and new example

--- examples/barwitherr.m
+++ examples/barwitherr.m
@@ -9,14 +9,19 @@
 %   varargin - parameters as passed to conventional bar plot
 %   See bar and errorbar documentation for more details.
 %
+%   Output:
+%   [hBar hErrorbar] = barwitherr(..) returns a vector of handles to the 
+%                      barseries (hBar) and error bar (hErrorbar) objects
+%
 %   Symmetric Example:
 %   y = randn(3,4);         % random y values (3 groups of 4 parameters) 
 %   errY = 0.1.*y;          % 10% error
-%   barwitherr(errY, y);    % Plot with errorbars
+%   h = barwitherr(errY, y);% Plot with errorbars
 %
 %   set(gca,'XTickLabel',{'Group A','Group B','Group C'})
 %   legend('Parameter 1','Parameter 2','Parameter 3','Parameter 4')
 %   ylabel('Y Value')
+%   set(h(1),'FaceColor','k');
 %
 %
 %   Asymmetric Example:
@@ -47,10 +52,23 @@
 %   24/10/2011  Martina F. Callaghan    Updated for asymmetric errors
 %   15/11/2011  Martina F. Callaghan    Fixed bug for assymetric errors &
 %                                       vector plots
+%   14/06/2013  Martina F. Callaghan    Returning handle as recommended by
+%                                       Eric (see submission comments)
+%   08/07/2013  Martina F. Callaghan    Only return handle if requested.
+%   18/07/2013  Martina F. Callaghan    Bug fix for single group data that 
+%                                       allows assymetric errors.
+%                                       Also removed dot from display as
+%                                       per Charles Colin comment. The 
+%                                       handle can be returned to control
+%                                       appearance.
+%   27/08/2013  Martina F. Callaghan    Ensuring errors are always stored
+%                                       as lowerErrors and upperErrors even
+%                                       if symmetric.
+%   29/10/2014  Martina F. Callaghan    Updated for 2014b graphics
 %
 %**************************************************************************
 
-function barwitherr(errors,varargin)
+function varargout = barwitherr(errors,varargin)
 
 % Check how the function has been called based on requirements for "bar"
 if nargin < 3
@@ -85,6 +103,7 @@ else
     lowerErrors = errors;
     upperErrors = errors;
 end
+
     
 % Check that the size of "errors" corresponsds to the size of the y-values.
 % Arbitrarily using lower errors as indicative.
@@ -95,19 +114,44 @@ end
 [nRows nCols] = size(values);
 handles.bar = bar(varargin{:}); % standard implementation of bar fn
 hold on
+hBar = handles.bar;
 
 if nRows > 1
+    hErrorbar = zeros(1,nCols);
     for col = 1:nCols
         % Extract the x location data needed for the errorbar plots:
-        x = get(get(handles.bar(col),'children'),'xdata');
+        if verLessThan('matlab', '8.4')
+            % Original graphics:
+            x = get(get(handles.bar(col),'children'),'xdata');
+        else
+            % New graphics:
+            x =  handles.bar(col).XData + [handles.bar(col).XOffset];
+        end
         % Use the mean x values to call the standard errorbar fn; the
         % errorbars will now be centred on each bar; these are in ascending
         % order so use xOrder to ensure y values and errors are too:
-        errorbar(mean(x,1),values(xOrder,col),lowerErrors(xOrder,col), upperErrors(xOrder, col), '.k')
+        hErrorbar(col) = errorbar(mean(x,1), values(xOrder,col), lowerErrors(xOrder,col), upperErrors(xOrder, col), '.k');
+        set(hErrorbar(col), 'marker', 'none')
     end
 else
-    x = get(get(handles.bar,'children'),'xdata');
-    errorbar(mean(x,1),values,errors,'.k')
+    if verLessThan('matlab', '8.4')
+        % Original graphics:
+        x = get(get(handles.bar,'children'),'xdata');
+    else
+       % New graphics:
+       x =  handles.bar.XData + [handles.bar.XOffset];
+   end
+    
+    hErrorbar = errorbar(mean(x,1), values, lowerErrors, upperErrors, '.k');
+    set(hErrorbar, 'marker', 'none')
 end
 
 hold off
+
+switch nargout
+    case 1
+        varargout{1} = hBar;
+    case 2
+        varargout{1} = hBar;
+        varargout{2} = hErrorbar;
+end
---
+++ examples/compare_probs_observer.m
@@ -0,0 +1,48 @@
+function compare_probs_observer( MM, n_obs, C )
+% Plots the probability of selecting any condition as better for all of the
+% data and a selected observer. Ideally used with outlier analysis
+%
+% MM - responses for each observer in seperate rows.
+% n_obs - number of observer to analyse
+% C - cell array with the labels used for each plotted conditions
+%
+
+% Compute probabilities per observer
+
+N = sqrt(size(MM,2));
+
+for oo=1:size(MM,1),
+    M = reshape(MM(oo,:),N,N);
+    
+    prob = M./(M + M');
+    prob(isnan(prob)) = 0;
+        
+    s_columns = sum(prob,1);
+    s_rows = sum(prob,2);
+        
+    data_boxplot_rows(oo,:) = s_rows'./(s_columns + s_rows');    
+end
+
+% Save the data from the outliers
+o_rows = data_boxplot_rows(n_obs,:);    
+
+% Remove the data from the outlier from the distribution
+data_boxplot_rows(n_obs,:) = [];
+%html_change_figure_print_size( gcf, 18, 8 );
+
+figure;
+hold on;
+% Plot the data for that scene
+boxplot(data_boxplot_rows,C)
+plot([1:N],o_rows,'ok','Linewidth',2)
+
+xlabel('Conditions compared')
+ylabel('Probability of higher quality')
+legend('Potential outlier')
+%html_change_figure_print_size( gcf, 20, 8 );
+grid on
+
+hold off
+
+
+end
