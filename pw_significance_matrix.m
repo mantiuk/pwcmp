7794d2067bd36d9f379014af9b29818e48e11597
Rafal Mantiuk <mantiuk@gmail.com>
2023-05-16 15:24:30 +0100

    Added documentation

--- pw_significance_matrix.m
+++ pw_significance_matrix.m
@@ -1,4 +1,15 @@
 function [p, D] = pw_significance_matrix( jod, pw_stats )
+% Get the p-values for the two-tailed z-test between all pairs of
+% conditions.
+%
+% See Section 7.2 "Statistical difference between two conditions" in
+% Perez-Ortiz, Maria, and Rafal K. Mantiuk. 
+% “A Practical Guide and Software for Analysing Pairwise Comparison Experiments.” 
+% ArXiv Preprint, December 11, 2017. http://arxiv.org/abs/1712.03686.
+%
+% Example:
+% [R, Rs] = pw_scale_table( D, [], { 'condition_1', 'condition_2' }, 'user_id', 'selection' );
+% [p, D] = pw_significance_matrix( R.jod, Rs{1}.stats ); 
 
 c = pw_stats.jod_cov;
 N = size(c,1);
